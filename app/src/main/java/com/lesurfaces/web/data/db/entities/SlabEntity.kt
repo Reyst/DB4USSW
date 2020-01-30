38ea8840a401d0bdce35621b15d9b5baa05e2eee
Reyst <oleksenko.pm@gmail.com>
2020-01-30 10:20:50 +0200

    fix primary key

--- app/src/main/java/com/lesurfaces/web/data/db/entities/SlabEntity.kt
+++ app/src/main/java/com/lesurfaces/web/data/db/entities/SlabEntity.kt
@@ -6,14 +6,13 @@ import com.lesurfaces.web.data.db.entities.OperationEntity
 @Entity(
     tableName = "operation_items",
     foreignKeys = [ForeignKey(entity = OperationEntity::class, parentColumns = ["id"], childColumns = ["operation_id"])],
-    indices = [Index(value = ["code", "operation_id"], unique = true)]
+    primaryKeys = ["code", "operation_id"]
 )
 data class SlabEntity(
 
     @ColumnInfo(name = "operation_id", index = true)
     val operationId: Long,
 
-    @PrimaryKey
     val code: String,
 
     val number: Int = 0,
